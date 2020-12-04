81869c336b493fd3a1d2cdfd00d0625052e046ac
andrey15253545 <chapelnuk98@mail.ru>
2020-12-04 20:33:43 +0300

    storage doc added

--- src/common/storage/impl/EasyStorage.java
+++ src/common/storage/impl/EasyStorage.java
@@ -7,10 +7,22 @@ import common.storage.Storage;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Allows to store only one implementation of a specific type
+ */
 public class EasyStorage implements Storage {
 
     private final Map<Class, Object> beanMap = new HashMap<>();
 
+
+    /**
+     * Adds bean to storage
+     *
+     * @param clazz  type of class to add to the storage
+     * @param object  instance to add to the storage
+     *
+     * @throws AdditionSameBeanException if an instance of the class being added exists
+     */
     @Override
     public void add(Class clazz, Object object) {
         if (beanMap.containsKey(clazz)) {
@@ -19,6 +31,14 @@ public class EasyStorage implements Storage {
         beanMap.put(clazz, object);
     }
 
+
+    /**
+     * Returns the stored instance by type
+     *
+     * @param beanClass type of requested instance
+     * @return instance of requested type
+     * @throws InstanceNotExistException if requested instance is not exists in the storage
+     */
     @Override
     public Object get(Class beanClass) {
         if (!beanMap.containsKey(beanClass)) {
